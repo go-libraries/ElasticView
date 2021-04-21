fb9c7d34eea895810b1fbd2ebc61da8a648ccb1f
1340691923 <1340691923@qq.com>
2021-04-22 01:45:51 +0800

    修改readme

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
-# **ElasticView**
+﻿  
 
-主要技术栈:golang + vue
+技术栈主要用： Vue + golang 
+
+欢迎大家提出自己的issue。
+
+ElasticView
+-----------
+[![Go Documentation](http://img.shields.io/badge/go-documentation-blue.svg?style=flat-square)](https://godoc.org/github.com/360EntSecGroup-Skylar/ElasticHD/main)
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg?maxAge=2592000)](https://github.com/1340691923/ElasticView/blob/main/LICENSE)
+[![Release](https://img.shields.io/github/release/360EntSecGroup-Skylar/ElasticHD.svg?label=Release)](https://github.com/360EntSecGroup-Skylar/ElasticHD/releases/latest)
+> ElasticView 是一款用来监控elasticsearch状态和操作elasticsearch索引的web可视化工具。它由golang开发而成，具有部署方便，占用内存小等优点
+ * es连接树管理（更方便的切换测试/生产环境）
+ * 支持页面级别的权限管理
+ * 支持sql转换成dsl语法
+ * 更方便的重建索引
+ * 任务管理
+ * 备份管理
+ * 可下载查询内容
+ * 可进行索引创建，映射创建，别名创建，索引删除等操作
  
+## ElasticView 部分截图
+
+![image](https://raw.githubusercontent.com/1340691923/ElasticView/main/show_img/1.png)
+![image](https://raw.githubusercontent.com/1340691923/ElasticView/main/show_img/2.png)
+![image](https://raw.githubusercontent.com/1340691923/ElasticView/main/show_img/3.png)
+![image](https://raw.githubusercontent.com/1340691923/ElasticView/main/show_img/4.png)
+![image](https://raw.githubusercontent.com/1340691923/ElasticView/main/show_img/5.png)
+![image](https://raw.githubusercontent.com/1340691923/ElasticView/main/show_img/6.png)
+![image](https://raw.githubusercontent.com/1340691923/ElasticView/main/show_img/7.png)
+
+## 应用程序下载
+[下载地址]( https://github.com/1340691923/ElasticView/releases/) 支持操作系统：windows，linux
+
+## 安装教程
+
+ 第一步: 下载 对应系统的ElasticView.zip
+ 第二步: 解压 ElasticView.zip，新建mysql数据库 es_view 导入es_view.sql 
+ 第三步: 修改config.json文件中的 数据库连接信息，日志存放目录和应用启动端口等配置信息
+ 第四步：windows：双击ElasticView.exe  linux：chmod +x ElasticView && nohup ./ElasticView 
+ 第五步：浏览器访问对应ip:端口，初始化用户名：admin，密码：admin
+ 
+ ## 手动编译
+ 第一步：git clone git@github.com:1340691923/ElasticView.git
+ 第二步：cd views && cnpm install (安装前端依赖)
+ 第三步：npm run build:stage （打前端正式包）
+ 第四步：cd .. &&   statik -src=views/dist -f (打包静态资源进二进制)
+ 第五步：windows: go build -o ElasticView.exe`|| linux : 双击build.bat // 编译go可执行程序 ``
--- sql/es_view.sql
+++ sql/es_view.sql
@@ -1,108 +1,105 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : 测试服
-Source Server Version : 50727
-Source Host           : 192.168.1.236:3306
-Source Database       : es_view
+ Source Server         : local
+ Source Server Type    : MySQL
+ Source Server Version : 50717
+ Source Host           : localhost:3306
+ Source Schema         : es_view
 
-Target Server Type    : MYSQL
-Target Server Version : 50727
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50717
+ File Encoding         : 65001
 
-Date: 2021-04-13 19:17:18
+ Date: 22/04/2021 01:31:30
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for `es_link`
+-- Table structure for es_link
 -- ----------------------------
 DROP TABLE IF EXISTS `es_link`;
-CREATE TABLE `es_link` (
+CREATE TABLE `es_link`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `ip` varchar(100) NOT NULL,
-  `user` varchar(100) NOT NULL,
-  `pwd` varchar(100) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `remark` varchar(100) DEFAULT '默认连接',
-  `version` tinyint(10) NOT NULL DEFAULT '6',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `es_remark` (`remark`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+  `ip` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `user` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `pwd` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `created` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
+  `remark` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '默认连接',
+  `version` tinyint(10) NOT NULL DEFAULT 6,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `es_remark`(`remark`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of es_link
 -- ----------------------------
+INSERT INTO `es_link` VALUES (10, 'http://127.0.0.1:9200', '', '', '2021-04-10 22:33:03', '2021-04-10 22:33:03', '测试', 6);
 
 -- ----------------------------
--- Table structure for `gm_dsl_history`
+-- Table structure for gm_dsl_history
 -- ----------------------------
 DROP TABLE IF EXISTS `gm_dsl_history`;
-CREATE TABLE `gm_dsl_history` (
+CREATE TABLE `gm_dsl_history`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `uid` int(11) DEFAULT '0',
-  `method` varchar(50) DEFAULT '',
-  `path` varchar(255) DEFAULT '',
-  `body` text,
-  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=829 DEFAULT CHARSET=utf8mb4;
+  `uid` int(11) DEFAULT 0,
+  `method` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '',
+  `path` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '',
+  `body` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+  `created` timestamp(0) DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 42 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of gm_dsl_history
--- ----------------------------
-
--- ----------------------------
--- Table structure for `gm_guid`
+-- Table structure for gm_guid
 -- ----------------------------
 DROP TABLE IF EXISTS `gm_guid`;
-CREATE TABLE `gm_guid` (
+CREATE TABLE `gm_guid`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `uid` int(11) NOT NULL,
-  `guid_name` varchar(100) NOT NULL,
-  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `guid_name` (`uid`,`guid_name`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8mb4;
+  `guid_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `created` timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP(0),
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `guid_name`(`uid`, `guid_name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of gm_guid
--- ----------------------------
-
--- ----------------------------
--- Table structure for `gm_role`
+-- Table structure for gm_role
 -- ----------------------------
 DROP TABLE IF EXISTS `gm_role`;
-CREATE TABLE `gm_role` (
+CREATE TABLE `gm_role`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `role_name` varchar(255) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `role_list` text,
+  `role_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `role_list` text CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of gm_role
 -- ----------------------------
-INSERT INTO `gm_role` VALUES ('1', 'admin', '超级管理员', '[{\"path\":\"/permission\",\"component\":\"layout\",\"redirect\":\"/permission/index\",\"alwaysShow\":true,\"meta\":{\"title\":\"权限\",\"icon\":\"el-icon-user-solid\"},\"children\":[{\"path\":\"role\",\"component\":\"views/permission/role\",\"name\":\"RolePermission\",\"meta\":{\"title\":\"角色管理\",\"icon\":\"el-icon-s-check\"}},{\"path\":\"user\",\"component\":\"views/permission/user\",\"name\":\"user\",\"meta\":{\"title\":\"用户管理\",\"icon\":\"el-icon-user\"}}]},{\"path\":\"/connect-tree\",\"component\":\"layout\",\"redirect\":\"/connect-tree/index\",\"alwaysShow\":false,\"meta\":{\"title\":\"连接树管理\",\"icon\":\"el-icon-link\"},\"children\":[{\"path\":\"/connect-tree/index\",\"component\":\"views/connect-tree/index\",\"name\":\"index\",\"meta\":{\"title\":\"连接树管理\",\"icon\":\"el-icon-link\"}}]},{\"path\":\"/cat\",\"component\":\"layout\",\"redirect\":\"/cat/index\",\"alwaysShow\":false,\"meta\":{\"title\":\"ES状态\",\"icon\":\"el-icon-pie-chart\"},\"children\":[{\"path\":\"/cat/index\",\"component\":\"views/cat/index\",\"name\":\"index\",\"meta\":{\"title\":\"ES状态\",\"icon\":\"el-icon-pie-chart\"}}]},{\"path\":\"/rest\",\"component\":\"layout\",\"redirect\":\"/rest/index\",\"alwaysShow\":false,\"meta\":{\"title\":\"开发工具\",\"icon\":\"el-icon-edit\"},\"children\":[{\"path\":\"/rest/index\",\"component\":\"views/rest/index\",\"name\":\"index\",\"meta\":{\"title\":\"开发工具\",\"icon\":\"el-icon-search\"}}]},{\"path\":\"/indices\",\"component\":\"layout\",\"redirect\":\"/indices/index\",\"alwaysShow\":true,\"meta\":{\"title\":\"索引管理\",\"icon\":\"el-icon-coin\"},\"children\":[{\"path\":\"index\",\"component\":\"views/indices/index\",\"name\":\"index\",\"meta\":{\"title\":\"索引管理\",\"icon\":\"el-icon-coin\"}},{\"path\":\"reindex\",\"component\":\"views/indices/reindex\",\"name\":\"reindex\",\"meta\":{\"title\":\"重建索引\",\"icon\":\"el-icon-document-copy\"}}]},{\"path\":\"/task\",\"component\":\"layout\",\"redirect\":\"/task/index\",\"alwaysShow\":false,\"meta\":{\"title\":\"任务\",\"icon\":\"el-icon-notebook-2\"},\"children\":[{\"path\":\"/task/index\",\"component\":\"views/task/index\",\"name\":\"index\",\"meta\":{\"title\":\"任务\",\"icon\":\"el-icon-notebook-2\"}}]},{\"path\":\"/back-up\",\"component\":\"layout\",\"redirect\":\"/back-up/index\",\"alwaysShow\":true,\"meta\":{\"title\":\"备份\",\"icon\":\"el-icon-copy-document\"},\"children\":[{\"path\":\"index\",\"component\":\"views/back-up/index\",\"name\":\"index\",\"meta\":{\"title\":\"快照存储库\",\"icon\":\"el-icon-first-aid-kit\"}},{\"path\":\"snapshot\",\"component\":\"views/back-up/snapshot\",\"name\":\"index\",\"meta\":{\"title\":\"快照管理\",\"icon\":\"el-icon-shopping-bag-2\"}}]}]');
+INSERT INTO `gm_role` VALUES (1, 'admin', '超级管理员', '[{\"path\":\"/permission\",\"component\":\"layout\",\"redirect\":\"/permission/index\",\"alwaysShow\":true,\"meta\":{\"title\":\"权限\",\"icon\":\"el-icon-user-solid\"},\"children\":[{\"path\":\"role\",\"component\":\"views/permission/role\",\"name\":\"RolePermission\",\"meta\":{\"title\":\"角色管理\",\"icon\":\"el-icon-s-check\"}},{\"path\":\"user\",\"component\":\"views/permission/user\",\"name\":\"user\",\"meta\":{\"title\":\"用户管理\",\"icon\":\"el-icon-user\"}}]},{\"path\":\"/connect-tree\",\"component\":\"layout\",\"redirect\":\"/connect-tree/index\",\"alwaysShow\":false,\"meta\":{\"title\":\"连接树管理\",\"icon\":\"el-icon-link\"},\"children\":[{\"path\":\"/connect-tree/index\",\"component\":\"views/connect-tree/index\",\"name\":\"index\",\"meta\":{\"title\":\"连接树管理\",\"icon\":\"el-icon-link\"}}]},{\"path\":\"/cat\",\"component\":\"layout\",\"redirect\":\"/cat/index\",\"alwaysShow\":false,\"meta\":{\"title\":\"ES状态\",\"icon\":\"el-icon-pie-chart\"},\"children\":[{\"path\":\"/cat/index\",\"component\":\"views/cat/index\",\"name\":\"index\",\"meta\":{\"title\":\"ES状态\",\"icon\":\"el-icon-pie-chart\"}}]},{\"path\":\"/rest\",\"component\":\"layout\",\"redirect\":\"/rest/index\",\"alwaysShow\":false,\"meta\":{\"title\":\"开发工具\",\"icon\":\"el-icon-edit\"},\"children\":[{\"path\":\"/rest/index\",\"component\":\"views/rest/index\",\"name\":\"index\",\"meta\":{\"title\":\"开发工具\",\"icon\":\"el-icon-search\"}}]},{\"path\":\"/indices\",\"component\":\"layout\",\"redirect\":\"/indices/index\",\"alwaysShow\":true,\"meta\":{\"title\":\"索引管理\",\"icon\":\"el-icon-coin\"},\"children\":[{\"path\":\"index\",\"component\":\"views/indices/index\",\"name\":\"index\",\"meta\":{\"title\":\"索引管理\",\"icon\":\"el-icon-coin\"}},{\"path\":\"reindex\",\"component\":\"views/indices/reindex\",\"name\":\"reindex\",\"meta\":{\"title\":\"重建索引\",\"icon\":\"el-icon-document-copy\"}}]},{\"path\":\"/task\",\"component\":\"layout\",\"redirect\":\"/task/index\",\"alwaysShow\":false,\"meta\":{\"title\":\"任务\",\"icon\":\"el-icon-notebook-2\"},\"children\":[{\"path\":\"/task/index\",\"component\":\"views/task/index\",\"name\":\"index\",\"meta\":{\"title\":\"任务\",\"icon\":\"el-icon-notebook-2\"}}]},{\"path\":\"/back-up\",\"component\":\"layout\",\"redirect\":\"/back-up/index\",\"alwaysShow\":true,\"meta\":{\"title\":\"备份\",\"icon\":\"el-icon-copy-document\"},\"children\":[{\"path\":\"index\",\"component\":\"views/back-up/index\",\"name\":\"index\",\"meta\":{\"title\":\"快照存储库\",\"icon\":\"el-icon-first-aid-kit\"}},{\"path\":\"snapshot\",\"component\":\"views/back-up/snapshot\",\"name\":\"index\",\"meta\":{\"title\":\"快照管理\",\"icon\":\"el-icon-shopping-bag-2\"}}]}]');
 
 -- ----------------------------
--- Table structure for `gm_user`
+-- Table structure for gm_user
 -- ----------------------------
 DROP TABLE IF EXISTS `gm_user`;
-CREATE TABLE `gm_user` (
+CREATE TABLE `gm_user`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `role_id` int(11) DEFAULT NULL COMMENT '角色id',
-  `realname` varchar(255) DEFAULT '' COMMENT '真实姓名',
+  `realname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT '' COMMENT '真实姓名',
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE KEY `gm_user_username` (`username`) USING BTREE COMMENT '角色名唯一索引'
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
+  UNIQUE INDEX `gm_user_username`(`username`) USING BTREE COMMENT '角色名唯一索引'
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of gm_user
 -- ----------------------------
-INSERT INTO `gm_user` VALUES ('1', 'admin', '21232f297a57a5a743894a0e4a801fc3', '1', '肖文龙');
+INSERT INTO `gm_user` VALUES (1, 'admin', '21232f297a57a5a743894a0e4a801fc3', 1, '肖文龙');
+
+SET FOREIGN_KEY_CHECKS = 1;
